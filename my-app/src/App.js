71297d6a910b6102100172344b07779985d910bd
AndriyHusakov <116489043+AndriyHusakov@users.noreply.github.com>
2023-06-03 11:02:05 +0300

    Завершили розмітку

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="todo">
+        <h1>To-Do List</h1>
+        <div class="form">
+          <input type="text" />
+          <button>Add</button>
+        </div>
+        <ul>
+      
+          <li>
+            <span>Item</span>
+            <span class="delete"><i class="fa fa-trash"></i></span>
+          </li>
+          <li>
+            <span>Item</span>
+            <span class="delete"><i class="fa fa-trash"></i></span>
+          </li>
+          <li>
+            <span>Item</span>
+            <span class="delete"><i class="fa fa-trash"></i></span>
+          </li>
+        </ul>
+      </div>
   );
 }
 
